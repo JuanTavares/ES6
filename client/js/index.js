48acf1c28ea00729876bf3b261e5c45efc2d81c8
Juan Carlos <juantavares2006@hotmail.com>
2018-02-14 04:17:14 -0300

    Adicionando tudo ao tbody
    Com a linha da tabela criada, agora ela será adicionada ao escopo da tabela da index.html

--- client/js/index.js
+++ client/js/index.js
@@ -4,17 +4,18 @@ var campos = [
     document.querySelector('#valor'),
 ]
 
+var tbody = document.querySelector('table tbody');
+
 document.querySelector('.form').addEventListener('submit', function(event) {
 
-    event.preventDefault();
-
     var tr = document.createElement('tr');
-  
+
     campos.forEach(function(campo) {
-  
         var td = document.createElement('td');
         td.textContent = campo.value;
         tr.appendChild(td);
-  
-      })
-  });
+    });
+
+    tbody.appendChild(tr);
+
+});
