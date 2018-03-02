2cdd7a38ec36638e88b4057926b726d34601a9f3
Juan Carlos <juantavares2006@hotmail.com>
2018-03-02 15:02:51 -0300

    Modificando o instanciamento de objeto

--- client/js/app/models/Negociacao.js
+++ client/js/app/models/Negociacao.js
@@ -1,8 +1,8 @@
 class Negociacao {
-    constructor() {
+    constructor(data, quantidade, valor) {
 
-        this.data = new Date();
-        this.quantidade = 1;
-        this.valor = 0.0;
+        this.data = data;
+        this.quantidade = quantidade;
+        this.valor = valor;
     }
 }
