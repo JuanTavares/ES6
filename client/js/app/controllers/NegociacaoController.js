557efbaddd4909b5e6ee2caba486ce9ccc226ebc
Juan Carlos <juantavares2006@hotmail.com>
2018-04-02 23:58:39 -0300

    Corrigindo

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -13,18 +13,12 @@ class NegociacaoController {
     adiciona(event) {
         event.preventDefault();
 
-        let data = new DateHelper().textoParaData(this._inputData.value);
+        let helper = new DateHelper();
 
         let negociacao = new Negociacao(
-            data,
+            helper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
             this._inputValor.value
         );
-
-
-        console.log(negociacao);
-
-        console.log(diaMesAno);
-
     }
 }
