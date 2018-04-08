e11ec5c0541b6e020732c243e1f12ad462d1e58b
Juan Carlos <juantavares2006@hotmail.com>
2018-04-08 00:31:24 -0300

    Criando update

--- client/js/app/views/MensagemView.js
+++ client/js/app/views/MensagemView.js
@@ -6,10 +6,15 @@ class MensagemView {
 
     }
 
-    _template(modelo) {
+    _template(model) {
 
         return model.texto ? `<p class="alert alert-info">${model.texto}</p>` : `<p></p>`
 
+    }
+
+    update(model) {
+
+        this._elemento.innerHTML = this._template(model);
     }
 
 }
