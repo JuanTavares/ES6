726ec78c69f251426083648bcfd1f8a61ba51813
Juan Carlos <juantavares2006@hotmail.com>
2018-04-15 14:48:37 -0300

    Criando código de Factory

--- client/js/app/services/ProxyFactory.js
+++ client/js/app/services/ProxyFactory.js
@@ -4,7 +4,26 @@ class ProxyFactory {
 
         return new Proxy(objeto, {
             get(target, prop, receiver) {
-                
+                if(props.includes(prop) && ProxyFactory._isFunction(target[prop])) {
+
+                    return function() {
+
+                        console.log(`a propriedade "${prop}" foi interceptada`);
+                        Reflect.apply(target[prop], target, arguments);
+                        return acao(target);
+                    };
+                }
+
+                return Reflect.get(target, prop, receiver);
+            },
+
+            set(target, prop, value, receiver) {
+
+                if(props.includes(prop)) {
+                    target[prop] = value;
+                    acao(target);
+                }
+                return Reflect.set(target, prop, value, receiver);
             }
         })
     }
