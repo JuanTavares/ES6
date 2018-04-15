fa3677096f6ab4549589917904f43838962fbd5c
Juan Carlos <juantavares2006@hotmail.com>
2018-04-15 15:20:23 -0300

    Corrigindo controller e listaNegocieacoes

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -23,12 +23,9 @@ class NegociacaoController {
         event.preventDefault();
 
         this._listaNegociacoes.adiciona(this._criaNegociacao());
-
         this._mensagem.texto = `Negociação adicionada com sucesso!`;
-        this._mensagemView.update(this._mensagem);
+        
         this._limpaFormulario();
-
-        console.log(this._listaNegociacoes.negociacoes)
     }
 
     apaga() {
@@ -36,7 +33,6 @@ class NegociacaoController {
         this._listaNegociacoes.esvazia();
 
         this._mensagem.texto = `Lista de negociações apagada com sucesso!`;
-        this._mensagemView.update(this._mensagem);
     }
 
     _limpaFormulario() {
