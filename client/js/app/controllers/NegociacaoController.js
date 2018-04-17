dc51c989f978f1d9d8b2c1e4c63b0949f3041ca8
Juan Carlos <juantavares2006@hotmail.com>
2018-04-17 00:15:15 -0300

    Configurando alguns parâmetros da conexão

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -38,6 +38,13 @@ class NegociacaoController {
         /* executa */
         xhr.onreadystatechange = () => {
 
+            if (xhr.readyState == 4) {
+                if (xhr.status == 200) {
+                    console.log('Obtendo as negociações do servidor.')
+                } else {
+                    console.log('Não foi possível obter as negociações do servidor.')
+                }
+            }
         }
         xhr.send();
 
