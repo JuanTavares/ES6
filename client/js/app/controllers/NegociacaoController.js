335b481b22ffe57be252037e8fb4818cb4a98de5
Juan Carlos <juantavares2006@hotmail.com>
2018-04-18 00:33:59 -0300

    Transferindo responsabilidade de conexão ajax para NegociacaoService

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -30,36 +30,8 @@ class NegociacaoController {
 
     importaNegociacoes() {
 
-        let xhr = new XMLHttpRequest();
-
-        /* configurações */
-        xhr.open('GET', 'negociacoes/semana');
-
-        /* executa */
-        xhr.onreadystatechange = () => {
-
-            if (xhr.readyState == 4) {
-                if (xhr.status == 200) {
-                    console.log('Obtendo as negociações do servidor.')
-
-                    JSON.parse(xhr.responseText)
-                        .map(objeto => new Negociacao(
-                            new Date(objeto.data),
-                            objeto.quantidade,
-                            objeto.valor))
-                        .forEach(negociacao =>
-                            this._listaNegociacoes.adiciona(negociacao));
-                    this._mensagem.texto = 'Negociações importadas com sucesso.';
-
-                } else {
-
-                    console.log(xhr.responseText);
-                    this._mensagem.texto = 'Não foi possível obter as negociações.';
-
-                }
-            }
-        }
-        xhr.send();
+        let service = new NegociacaoService();
+        service.obterNegociacoesDaSemana;
 
     }
 
