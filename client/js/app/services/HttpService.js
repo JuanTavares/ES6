6c32b1c776591509b6995301a0707f6c24efc265
Juan Carlos <juantavares2006@hotmail.com>
2018-04-19 23:21:29 -0300

    Contruindo escopo da utilização do padrão Promise

--- client/js/app/services/HttpService.js
+++ client/js/app/services/HttpService.js
@@ -1,5 +1,17 @@
 class HttpService {
 
+    constructor() {
 
-    
+        this.http = new HttpService();
+    }
+
+    get(url) {
+
+        return new Promise((resolve, reject) => {
+
+            
+
+        })
+    }
+
 }
