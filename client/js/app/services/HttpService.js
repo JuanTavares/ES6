ac237daad6a79e75ee8cd6adca98bbb9dec64db1
Juan Carlos <juantavares2006@hotmail.com>
2018-04-21 14:43:09 -0300

    Completando a requisição POST

--- client/js/app/services/HttpService.js
+++ client/js/app/services/HttpService.js
@@ -6,7 +6,7 @@ class HttpService {
 
             let xhr = new XMLHttpRequest();
 
-             /* configurações */
+            /* configurações */
             xhr.open('GET', url);
 
             /* executa */
@@ -33,6 +33,22 @@ class HttpService {
             let xhr = new XMLHttpRequest();
             xhr.open('POST', url, true);
             xhr.setRequestHeader('Content-Type', 'application/json');
+
+            xhr.onreadystatechange = () => {
+
+                if (xhr.readyState == 4) {
+
+                    if (xhr.status == 200) {
+
+                        resolve(JSON.parse(xhr.responseText));
+                    } else {
+
+                        reject(xhr.responseText);
+                    }
+                }
+            };
+
+            xhr.send(JSON.stringify(dado));
 
         });
 
