593efe10e6bf0adf4c25dcc964d5fdf3c71d0a82
Juan Carlos <juantavares2006@hotmail.com>
2018-04-24 00:20:14 -0300

    Finalizando adiciona

--- client/js/app/dao/NegociacaoDao.js
+++ client/js/app/dao/NegociacaoDao.js
@@ -9,7 +9,27 @@ class NegociacaoDao {
 
     adiciona(negociacao) {
 
-        
+        return new Promise((resolve, reject) => {
+
+            let request = this._connection
+                .transaction([this._store], `readwrite`)
+                .objectStore(this._store)
+                .add(negociacao);
+
+            request.onsuccess = (e) => {
+
+                resolve();
+
+            }
+
+            request.onerror = (e) => {
+
+                console.log(e.target.error);
+                reject(`Não foi possível adicionar a negociação.`);
+
+            }
+
+        })
 
     }
 
