ee212ada40e137645c248563a1c385b2e18a9503
Juan Carlos <juantavares2006@hotmail.com>
2018-04-23 23:28:07 -0300

    Isolando connection factory em uma função autoinvocada

--- client/js/app/services/ConnectionFactory.js
+++ client/js/app/services/ConnectionFactory.js
@@ -1,39 +1,43 @@
-class ConnectionFactory {
+var ConnectionFactory = (function() {
 
-    constructor() {
 
-        throw new Error(`Não é possível instanciar ConnectionFactory`);
+    class ConnectionFactory {
+
+        constructor() {
+
+            throw new Error(`Não é possível instanciar ConnectionFactory`);
+        }
+
+        static getConnection() {
+
+            return new Promise((resolve, reject) => {
+
+                let openRequest = window.indexedDB.open(`NegociacoesDB`, 1);
+
+                /*Tríade de chamadas*/
+
+                openRequest.onupgradeneeded = e => {
+
+                }
+
+                openRequest.onsuccess = e => {
+
+                }
+
+                openRequest.onerror = e => {
+
+                }
+                /*Fim Tríade */
+
+            })
+
+        }
+
+        static _createStores(connection) {
+
+
+
+        }
+
     }
-
-    static getConnection() {
-
-        return new Promise((resolve, reject) => {
-
-            let openRequest = window.indexedDB.open(`NegociacoesDB`, 1);
-
-            /*Tríade de chamadas*/
-            
-            openRequest.onupgradeneeded = e => {
-
-            }
-
-            openRequest.onsuccess = e => {
-
-            }
-
-            openRequest.onerror = e => {
-
-            }
-            /*Fim Tríade */
-
-        })
-
-    }
-
-    static _createStores(connection) {
-
-        
-
-    }
-
-}
+})();
