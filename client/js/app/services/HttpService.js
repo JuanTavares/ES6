b02fe936b7ed982d80648cedbf61e81955133195
Juan Carlos <juantavares2006@hotmail.com>
2018-04-28 01:14:35 -0300

    Trocando XMLHttpRequest por FetchAPI

--- client/js/app/services/HttpService.js
+++ client/js/app/services/HttpService.js
@@ -2,27 +2,9 @@ class HttpService {
 
     get(url) {
 
-        return new Promise((resolve, reject) => {
-
-            let xhr = new XMLHttpRequest();
-
-            /* configurações */
-            xhr.open('GET', url);
-
-            /* executa */
-            xhr.onreadystatechange = () => {
-                if (xhr.readyState == 4) {
-                    if (xhr.status == 200) {
-                        resolve(JSON.parse(xhr.responseText));
-                    } else {
-                        console.log(xhr.responseText);
-                        reject(xhr.responseText);
-                    }
-                }
-            }
-
-            xhr.send();
-        });
+        return fetch(url)
+            .then(res => this._handleErrors(res))
+            .then(res => res.json());
 
     }
 
