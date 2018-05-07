74157458c8b0b2edee00570be8ebc2a8d6b69d4a
Juan Carlos <juantavares2006@hotmail.com>
2018-05-06 23:23:38 -0300

    Aplicando webpack.ProvidePlugin

--- Webpack/client/app-src/app.js
+++ Webpack/client/app-src/app.js
@@ -2,7 +2,8 @@ import { NegociacaoController } from './controllers/NegociacaoController.js';
 import { Negociacao } from './domain/index.js';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
-import '../css/meucss.css';
+import 'bootstrap/js/modal.js';
+import '../css/meuCss.css';
 
 const controller = new NegociacaoController();
 const negociacao = new Negociacao(new Date(), 1, 200);
